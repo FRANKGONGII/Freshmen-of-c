52b98d39f57b987d48ec2a70da37dda0f4b07661
GYJ <2354684632@qq.com>
2021-10-09 23:00:36 +0800

    add good-print

---
+++ good-print.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<stdlib.h>
+//int main()
+//{
+//	char arr1[] = "welcome";
+//	char arr2[] = "#######";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	for ( left = 0,  right = strlen(arr1)-1;left <= right; left++, right--)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);
+//		system("cls");
+//	}
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+// 
+// 
+// 以下为while版本
+// 
+// 
+// 
+//int main()
+//{
+//	char arr1[] = "welcome";
+//	char arr2[] = "#######";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//			printf("%s\n", arr2);
+//			Sleep(1000);
+//			system("cls");
+//			left++;
+//			right--;
+//	}
+//	printf("%s\n", arr1);
+//	return 0;
+
+
+//}
